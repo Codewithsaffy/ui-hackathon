3b35d5acafb4022c8717ba31c5299c9b49e386ed
Muhammad Yousuf <codewithsaffy@gmail.com>
2024-12-31 04:23:31 +0500

    add

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // set to true to fetch from edge cache
   token: process.env.SANITY_API_TOKEN,
 })
